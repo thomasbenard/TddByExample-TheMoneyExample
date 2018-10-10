ac33e0997265cd2cd452942a20e601f44b0a9080
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-10 19:01:00 +0200

    Red: Add first test for the multiplication

--- TODO.md
+++ TODO.md
@@ -2,3 +2,6 @@
 
 - $5 + 10 CHF = $10 if rate is 2:1
 - **$5 * 2 = $10**
+- Make "amount" private
+- Dollar side-effect ?
+- Money rounding ?
---
+++ src/test/java/MoneyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
