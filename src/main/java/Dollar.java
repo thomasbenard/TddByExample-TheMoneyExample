0a6176a2492218e33ddc1031329013cfc6d8a90c
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-11 15:08:06 +0200

    Refactor: Make Dollar.times compute the amount

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,11 +1,11 @@
 public class Dollar {
-    public int amount = 5 * 2;
+    public int amount;
 
     public Dollar(int amount) {
 
     }
 
     public void times(int multiplier) {
-
+        amount = 5 * 2;
     }
 }
