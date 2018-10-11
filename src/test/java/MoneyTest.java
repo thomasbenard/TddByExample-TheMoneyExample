d4c9a4e151c9039bf6f46629e3f5b091b267c434
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-11 19:01:52 +0200

    Red: Add test for currencies

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -26,4 +26,10 @@ public class MoneyTest {
         assertNotEquals(Money.franc(5), Money.franc(6));
         assertNotEquals(Money.franc(5), Money.dollar(5));
     }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
