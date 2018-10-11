c86cdefea07e7d396a0717248d1817438499692d
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-11 18:38:56 +0200

    Refactor: Add factory method Money.dollar

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,6 +1,10 @@
 public class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
