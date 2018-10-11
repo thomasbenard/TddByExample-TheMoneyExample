988f3648efb57d9361ecc5b33a64431fbad03175
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-11 17:41:55 +0200

    Red: multiplication for Franc should also work

--- TODO.md
+++ TODO.md
@@ -9,3 +9,4 @@
 - hashCode()
 - Equal null
 - Equal object
+- **5 CHF * 2 = 10 CHF**
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -12,6 +12,13 @@ public class MoneyTest {
     }
 
     @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     public void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
