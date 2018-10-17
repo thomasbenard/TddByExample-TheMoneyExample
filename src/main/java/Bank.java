885e6a9bad8fd26de6e710c57c0deca5deb7e2a4
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-17 13:20:50 +0200

    Refactor: Use polymorphism instead of class casting

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,6 +1,6 @@
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money) return (Money) source;
+        if (source instanceof Money) return (Money) source.reduce(to);
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
