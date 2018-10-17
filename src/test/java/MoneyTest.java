55cf9592470db41981ae3ec8d8230041abacebeb
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-17 13:27:16 +0200

    Red: Bank must reduce with different currencies

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -36,6 +36,14 @@ public class MoneyTest {
     }
 
     @Test
+    public void testReduceMoneyWithDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
     public void testPlusReturnsSum() {
         Money five = Money.dollar(5);
         Expression result = five.plus(five);
