f675b97f82d891ed01e99df178726930c7c985e1
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-17 13:00:57 +0200

    Red: If currencies are the same, the sum is the sum of the amounts

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -21,6 +21,14 @@ public class MoneyTest {
     }
 
     @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
     public void testPlusReturnsSum() {
         Money five = Money.dollar(5);
         Expression result = five.plus(five);
