c20daf1cddf47e1680b5c61632403fbcd53ae6d4
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-17 12:49:15 +0200

    Green: Make the test pass simply

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,4 +1,9 @@
-public class Sum {
+public class Sum implements Expression {
     public Money augend;
     public Money addend;
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
 }
