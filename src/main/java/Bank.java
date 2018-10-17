066a996212440576a165fb0175e23662a85a1497
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-17 13:21:28 +0200

    Refactor: Eliminate casts in Bank.reduce

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,7 +1,5 @@
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money) return (Money) source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
