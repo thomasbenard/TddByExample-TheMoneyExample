787731b957ee618e8cfa996facd7020b583c6f9e
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-18 12:59:01 +0200

    Red: we should be able to add a Sum and a Money

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -59,6 +59,17 @@ public class MoneyTest {
     }
 
     @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
+
+    @Test
     public void testPlusReturnsSum() {
         Money five = Money.dollar(5);
         Expression result = five.plus(five);
