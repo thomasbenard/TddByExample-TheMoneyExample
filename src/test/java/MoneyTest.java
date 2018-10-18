e044ffdaddde93e1573c22873cedb1071bb0278b
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-18 13:12:19 +0200

    Red: adding two Money with same currency should return a Money

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MoneyTest {
     @Test
@@ -78,6 +79,12 @@ public class MoneyTest {
         Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(20), result);
+    }
+
+    @Test
+    public void testPlusSameCurrencyReturnsMoney() {
+        Expression sum = Money.dollar(1).plus(Money.dollar(1));
+        assertTrue(sum instanceof Money);
     }
 
     @Test
