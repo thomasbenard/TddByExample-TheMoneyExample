4ee9662f25fac5c5cdaf2ee0c3db411a372f93f1
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-18 08:05:46 +0200

    Refactor: Use the Pair class in Bank. We broke the tests !

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,13 +1,18 @@
+import java.util.HashMap;
+
 public class Bank {
+
+    HashMap<Pair, Integer> rates = new HashMap<>();
+
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     public void addRate(String from, String to, int rate) {
-
+        rates.put(new Pair(from, to), rate);
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return rates.get(new Pair(from, to));
     }
 }
