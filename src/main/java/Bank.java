168d5123acfea8cbd2b8e781a70e82597e655e3b
thomasbenard <m.thomas.benard@protonmail.com>
2018-10-18 07:57:18 +0200

    Refactor: Expression.reduce should use the Bank to retrieve rates

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,9 +1,13 @@
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String from, String to, int rate) {
 
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
